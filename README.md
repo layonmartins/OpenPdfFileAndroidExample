21f4563ad2e48b84082843e791bea41e68c5d83f
Layon Martins <layonmartins03@gmail.com>
2020-12-17 14:24:49 -0300

    add listeners on MainActivity buttons and create README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+## Android app example of how to open PDF programmatically
+
+* Opening a PDF file using WebView
+* Opening a PDF file from assets using AndroidPdfViewer library
+* Opening a PDF file form storage using AndroidPdfViewer library
+* Opening a PDF file from the internet using AndroidPdfViewer library
+
+### Language: Kotlin
+
+![](Screenshot.jpg)
+
+### Reference
+* https://blog.mindorks.com/how-to-open-a-pdf-file-in-android-programmatically
--- app/src/main/java/com/layonf/open_pdf_file_android_example/MainActivity.kt
+++ app/src/main/java/com/layonf/open_pdf_file_android_example/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.layonf.open_pdf_file_android_example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpOnClickListener()
+    }
+
+    private fun setUpOnClickListener() {
+
+        buttonWebView.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonAssets.setOnClickListener {
+            val intent = Intent(this, PdfViewActivity::class.java)
+            intent.putExtra("ViewType", "assests")
+            startActivity(intent)
+        }
+
+        buttonStorage.setOnClickListener{
+            val intent = Intent(this, PdfViewActivity::class.java)
+            intent.putExtra("ViewType", "storage")
+            startActivity(intent)
+        }
+
+        buttonInternet.setOnClickListener {
+            val intent = Intent(this, PdfViewActivity::class.java)
+            intent.putExtra("ViewType", "intent")
+            startActivity(intent)
+        }
     }
 }
